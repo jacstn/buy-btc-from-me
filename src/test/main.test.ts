841626d509fa66bd17460ac44dd1a941b4fe2751
Jacek Stan <jacek.stanusz@gmail.com>
2022-07-16 20:35:47 +0200

    validate address tests

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -57,5 +57,25 @@ describe('POST /order', () => {
                 else done()
             })
     })
+})
 
+describe('GET /validate-address', () => {
+    it('should return OK, when address is valid', function (done) {
+        const address = 'tb1qz4dxjgu603khe0hdu65s8e75rnjud0zqrhlmj8';
+        request(app).get(`/validate-address/${address}`).send()
+            .expect(200)
+            .end((err: any, res: any) => {
+                if (err) done(err);
+                else done()
+            })
+    })
+    it('should return 404 on invalid address', function (done) {
+        const address = 'tb1qz4dxjgu603khe0';
+        request(app).get(`/validate-address/${address}`).send()
+            .expect(404)
+            .end((err: any, res: any) => {
+                if (err) done(err);
+                else done()
+            })
+    })
 })
