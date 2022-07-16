6a8f304e4875b3b5226b1435ed0bc07145818d08
Jacek Stan <jacek.stanusz@gmail.com>
2022-07-16 20:20:31 +0200

    more tests

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -8,18 +8,51 @@ describe('POST /order', () => {
         request(app).post('/order').send({
             "btcAmount": 0.01,
             "usdAmount": 0.01,
-            "address": "fa2323423423423423e423"
+            "address": "tb1qz4dxjgu603khe0hdu65s8e75rnjud0zqrhlmj8"
         })
             .expect(200)
             .expect(async (res: any) => {
                 const savedBtcAddress: string = JSON.parse(res.text).address;
 
-                const model: any = await models.Domain.findOne({ where: { address:savedBtcAddress }, raw: true });
+                const model: any = await models.Domain.findOne({ where: { address: savedBtcAddress }, raw: true });
 
                 expect(savedBtcAddress).toBe(model.address);
                 done();
             })
-            .end((err:any, res:any) => {
+            .end((err: any, res: any) => {
+                if (err) done(err);
+                else done()
+            })
+    })
+    it('should not create order when missing address', function (done) {
+        request(app).post('/order').send({
+            "btcAmount": 0.01,
+            "usdAmount": 0.01,
+        })
+            .expect(422)
+            .end((err: any, res: any) => {
+                if (err) done(err);
+                else done()
+            })
+    })
+    it('should not create order when missing USD amount', function (done) {
+        request(app).post('/order').send({
+            "btcAmount": 0.01,
+            "address": "tb1qz4dxjgu603khe0hdu65s8e75rnjud0zqrhlmj8"
+        })
+            .expect(422)
+            .end((err: any, res: any) => {
+                if (err) done(err);
+                else done()
+            })
+    })
+    it('should not create order when missing BTC amount', function (done) {
+        request(app).post('/order').send({
+            "usdAmount": 1000,
+            "address": "tb1qz4dxjgu603khe0hdu65s8e75rnjud0zqrhlmj8"
+        })
+            .expect(422)
+            .end((err: any, res: any) => {
                 if (err) done(err);
                 else done()
             })
